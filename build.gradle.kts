ca08501b03b2e2e43dd40a552724ff5be1109673
LifeForge <lifeforge@users.noreply.github.com>
2026-09-10 06:46:34 +0300

    Remove unused ksp/flyway plugin declarations that failed resolution

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,6 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.13" apply false
-    id("org.flywaydb.flyway") version "10.15.0" apply false
     id("com.squareup.sqldelight") version "2.0.1" apply false
 }
 
